aa100c107d43ca262eb49911a0de23bbd95813ba
Julian Scheuchenzuber <js@lvl51.de>
2020-10-01 13:16:21 +0200

    Add util class with request method constants

---
+++ src/FindHTTPUtil.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Level51\FindHTTPAction;
+
+/**
+ * @package Level51\FindHTTPAction
+ */
+class FindHTTPUtil {
+
+    const GET = 'GET';
+
+    const POST = 'POST';
+
+    const PUT = 'PUT';
+
+    const DELETE = 'DELETE';
+
+    const OPTIONS = 'OPTIONS';
+
+    const HEAD = 'HEAD';
+
+    const CONNECT = 'CONNECT';
+
+    const TRACE = 'TRACE';
+
+    const PATCH = 'PATCH';
+}
